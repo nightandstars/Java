cd7d2e66158345a42701fbe8b19f84b263dd1fa4
nightandstars <morgane.peysson@laposte.net>
2025-07-29 19:40:26 +0200

    Implemented functional startingMenu() to run the game

--- src/dnd/game/Main.java
+++ src/dnd/game/Main.java
@@ -2,6 +2,7 @@ package dnd.game;
 
 public class Main {
     public static void main(String[] args) {
-
+        Menu mainMenu = new Menu();
+        mainMenu.startingMenu();
     }
 }
--- src/dnd/game/Menu.java
+++ src/dnd/game/Menu.java
@@ -8,67 +8,76 @@ import java.util.Scanner;
 
 public class Menu {
 
-    public void startingMenu(){
-        Scanner myScanner = new Scanner(System.in);
+    Scanner myScanner = new Scanner(System.in);
+    Character chosenCharacter = null;
 
+    public void startingMenu(){
         System.out.println("Welcome to DND, pick an option below:");
         System.out.println("1 - Create a New Character");
         System.out.println("2 - Exit Game");
-
         int choice = myScanner.nextInt();
-        Character chosenCharacter = null;
 
         switch(choice){
             case 1:
-                System.out.println("Do you want a Wizard or a Warrior?:");
-                System.out.println("1 - Wizard");
-                System.out.println("2 - Warrior");
-                System.out.println("3 - Exit");
-                int characterType = myScanner.nextInt();
-                System.out.println("What is their name?");
-                String characterName = myScanner.nextLine();
-                switch (characterType){
-                    case 1:
-                         chosenCharacter = this.newCharacter("Wizard", characterName);
-                        break;
-                    case 2:
-                        chosenCharacter = this.newCharacter("Warrior", characterName);
-                        break;
-                    case 3:
-                        System.exit(0);
-                        break;
-                }
+                Character character = this.chosenCharacterClass();
                 break;
             case 2:
-                System.exit(0);
+                this.quitGame();
+        }
+        this.characterInfo();
+
         }
 
+    private Character chosenCharacterClass() {
+        System.out.println("Do you want a Wizard or a Warrior?:");
+        System.out.println("1 - Wizard");
+        System.out.println("2 - Warrior");
+        System.out.println("3 - Exit");
+        int characterType = myScanner.nextInt();
+        String characterName = this.chosenCharacterName();
+        switch (characterType) {
+            case 1:
+                return chosenCharacter = new Wizard(characterName);
+            case 2:
+                return chosenCharacter = new Warrior(characterName);
+            case 3:
+                this.quitGame();
+                break;
+        }
+        return chosenCharacter;
+    }
+
+    private void characterInfo(){
         System.out.println("Do you want to:");
         System.out.println("1 - See the character's info?");
         System.out.println("2 - Modify the info?");
         System.out.println("3 - Exit");
-        int choice2 = myScanner.nextInt();
-        switch(choice2){
+        int choice = myScanner.nextInt();
+        switch(choice){
             case 1:
                 System.out.println(chosenCharacter.toString());
                 break;
             case 2:
+                myScanner.nextLine();
                 System.out.println("What is the new name of your character?");
                 String newName = myScanner.nextLine();
                 chosenCharacter.setName(newName);
                 break;
             case 3:
-                System.exit(0);
-        }
-
-        }
-
-    private Character newCharacter(String characterType, String name){
-        if(characterType.equals("Wizard")){
-            return new Wizard(name);
-        }else{
-            return new Warrior(name);
+                this.quitGame();
         }
     }
 
+    public void quitGame(){
+        System.exit(0);
+    }
+
+    private String chosenCharacterName(){
+        myScanner.nextLine();
+        System.out.println("What is their name?");
+        String characterName = myScanner.nextLine();
+        return characterName;
+    }
 }
+
+
